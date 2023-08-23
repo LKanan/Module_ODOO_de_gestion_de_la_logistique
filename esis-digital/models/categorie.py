aee5c70297415cf7ae338b489d8c5f892daefbdb
L_Kanan <127052913+Lkanan@users.noreply.github.com>
2023-08-23 11:33:59 +0200

    categorie.py

--- esis-digital/models/categorie.py
+++ esis-digital/models/categorie.py
@@ -7,5 +7,5 @@ class Esis_digitalCategorie(models.Model):
     _name = 'esis_digital.categorie'
     _description = 'Gestion des categories'
 
-    name = fields.Char()
+    name = fields.Char(string='Liste des categories')
     materiel_id = fields.Many2one('esis_digital.materiel')
