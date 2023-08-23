8ec98bbce0aed08912f70299b34a13f477f4d95b
L_Kanan <127052913+Lkanan@users.noreply.github.com>
2023-08-23 18:52:03 +0200

    apport.py

--- esis-digital/models/rapport.py
+++ esis-digital/models/rapport.py
@@ -11,23 +11,23 @@ class Esis_digitalRapport(models.Model):
     commentaires = fields.Text()
     create_date = fields.Datetime(string="Date de redaction", readonly=True)
 
-    # @api.onchange()
-    # def send_mail(self):
-    #     self.ensure_one()
-    #     template_id = self.env.ref('esis-digital.email_effect_dmd').id
-    #     ctx = {
-    #         'default_model': 'esis_digital.effect_dmd',
-    #         'default_res_id': self.id,
-    #         'default_use_template': bool(template_id),
-    #         'default_template_id': template_id,
-    #         'default_composition_mode': 'comment',
-    #         # 'email_to': self.email,
-    #     }
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'view_type': 'form',
-    #         'view_mode': 'form',
-    #         'res_model': 'mail.compose.message',
-    #         'target': 'new',
-    #         'context': ctx,
-    #     }
+    @api.onchange()
+    def send_mail(self):
+        self.ensure_one()
+        template_id = self.env.ref('esis-digital.email_effect_dmd').id
+        ctx = {
+            'default_model': 'esis_digital.effect_dmd',
+            'default_res_id': self.id,
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            # 'email_to': self.email,
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'target': 'new',
+            'context': ctx,
+        }
